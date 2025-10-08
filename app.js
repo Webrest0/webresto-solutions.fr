d7b90dce8cc57f0cc590aa0edabc58f3264a546e
Webrest0 <smarttlelearning@gmail.com>
2025-10-08 16:50:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// menu mobile
+// Menu mobile simple
 const burger = document.getElementById('burger');
 burger?.addEventListener('click', () => {
   const nav = document.querySelector('.nav');
-  if (!nav) return;
+  if(!nav) return;
   nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
 });
